ba3bfd6c49c384b8f8bfe089cee88789aaace351
Stefan Janse van Rensburg <stefanvr250@gmail.com>
2025-10-03 14:51:34 +0200

    Installing a routing package (chi v5)

--- fullstack-Go-Watch-a-Movie/backend/cmd/api/main.go
+++ fullstack-Go-Watch-a-Movie/backend/cmd/api/main.go
@@ -25,9 +25,9 @@ func main() {
 
 	log.Println("starting application on port:", port)
 
-	http.HandleFunc("/", Hello)
+	
 	// start a web server
-	err := http.ListenAndServe(fmt.Sprintf(":%d", port), nil)
+	err := http.ListenAndServe(fmt.Sprintf(":%d", port), app.routes())
 	if err != nil {
 		log.Fatal(err)
 	}
