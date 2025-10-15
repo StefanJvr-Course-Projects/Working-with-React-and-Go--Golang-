8fdac34d78a6c17789c45f9614ef04458a680dc8
Stefan Janse van Rensburg <stefanvr250@gmail.com>
2025-10-15 15:41:31 +0200

    Connecting the front end to the back end API

--- fullstack-Go-Watch-a-Movie/frontend/src/components/Movies.js
+++ fullstack-Go-Watch-a-Movie/frontend/src/components/Movies.js
@@ -5,26 +5,22 @@ const Movies = () => {
   const [movies, setMovies] = useState([]);
 
   useEffect(() => {
-    let moviesList = [
-      {
-        id: 1,
-        title: "Highlander",
-        release_date: "1986-03-07",
-        runtime: 116,
-        mpaa_rating: "R",
-        descrition: "Some long description",
-      },
-      {
-        id: 2,
-        title: "Raiders of the Lost Ark",
-        release_date: "1981-06-12",
-        runtime: 115,
-        mpaa_rating: "PG-13",
-        descrition: "Some long description",
-      },
-    ];
+      const headers = new Headers()
+      headers.append("Content-Type","application/json");
 
-    setMovies(moviesList);
+      const requestOptions = {
+        method: "GET",
+        headers: headers,
+      }
+
+      fetch(`http://localhost:8080/movies`, requestOptions)
+        .then((response) => response.json())
+        .then((data) => {
+          setMovies(data);
+        })
+        .catch(err => {
+          console.log(err);
+        })
   }, []);
 
   return (
